70cf648c763302d5080f65f5df200e8bd4c80dde
Charlie-Steer <gruiz.carlos@gmail.com>
2024-01-30 00:44:22 +0100

    Managed to reduce split() to 25 lines and pass Norminette.

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: cargonz2 <cargonz2@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 15:14:37 by cargonz2          #+#    #+#             */
-/*   Updated: 2024/01/30 00:03:02 by cargonz2         ###   ########.fr       */
+/*   Updated: 2024/01/30 00:42:27 by cargonz2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,20 +52,29 @@ static size_t	get_word_len(const char *s, char c)
 	return (word_len);
 }
 
-static char **allocate_string_array()
-
-static size_t	allocate_string(char **str_arr, char const *s, int j, size_t i, size_t word_len)
+static char	**allocate_string_array(char const *s, char c)
 {
-	str_arr[j] = ft_substr(s, i - word_len, word_len);
+	size_t	word_count;
+	char	**str_arr;
+
+	word_count = count_words(s, c);
+	str_arr = malloc(sizeof(char *) * (word_count + 1));
+	return (str_arr);
+}
+
+static size_t	allocate_string(char **str_arr, char const *s,
+				size_t word_len, int j)
+{
+	str_arr[j] = ft_substr(s, 0, word_len);
 	if (str_arr[j] == NULL)
 	{
-	while (j > 0)
+		while (j > 0)
 		{
 			free(str_arr[j - 1]);
 			j--;
 		}
-	free(str_arr);
-	return (-1);
+		free(str_arr);
+		return (-1);
 	}
 	j++;
 	word_len = 0;
@@ -74,17 +83,14 @@ static size_t	allocate_string(char **str_arr, char const *s, int j, size_t i, si
 
 char	**ft_split(char const *s, char c)
 {
-	size_t	word_count;
 	size_t	word_len;
 	size_t	i;
 	int		j;
 	char	**str_arr;
 
-	word_count = count_words(s, c);
-	word_len = 0;
 	i = 0;
 	j = 0;
-	str_arr = malloc(sizeof(char *) * (word_count + 1));
+	str_arr = allocate_string_array(s, c);
 	if (str_arr == NULL)
 		return (NULL);
 	while (s[i] != '\0')
@@ -95,7 +101,7 @@ char	**ft_split(char const *s, char c)
 		i += word_len;
 		if (word_len != 0)
 		{
-			j = allocate_string(str_arr, s, j, i, word_len);
+			j = allocate_string(str_arr, &s[i - word_len], word_len, j);
 			if (j == -1)
 				return (NULL);
 		}
