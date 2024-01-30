38d718b1f41c11677b577c488535881f12d72e1a
Charlie-Steer <gruiz.carlos@gmail.com>
2024-01-30 04:27:44 +0100

    Solved lstmap() memory issues! Everything done?

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: cargonz2 <cargonz2@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/22 14:07:01 by cargonz2          #+#    #+#             */
-/*   Updated: 2024/01/29 21:18:45 by cargonz2         ###   ########.fr       */
+/*   Updated: 2024/01/30 04:26:00 by cargonz2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,18 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*node;
 	t_list	*newlist;
+	void	*f_content;
 
 	if (lst == NULL || f == NULL || del == NULL)
 		return (NULL);
 	newlist = NULL;
 	while (lst != NULL)
 	{
-		node = ft_lstnew((*f)(lst->content));
+		f_content = (*f)(lst->content);
+		node = ft_lstnew(f_content);
 		if (node == NULL)
 		{
+			del(f_content);
 			ft_lstclear(&newlist, del);
 			return (NULL);
 		}
